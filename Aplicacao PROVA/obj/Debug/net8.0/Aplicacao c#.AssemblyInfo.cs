b4427b287192c89522b9f75167271b81ccf4936c
Elizeu Freitas <elizeuferreiradefreitas@gmail.com>
2024-03-08 12:20:35 -0300

    Forçar a detecção de linguagem

--- Aplicacao PROVA/obj/Debug/net8.0/Aplicacao c#.AssemblyInfo.cs
+++ Aplicacao PROVA/obj/Debug/net8.0/Aplicacao c#.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aplicacao c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a23aa151253a81ca88a625e1839bec4bfb9b7ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf58ad084753c972a5d9b124b0ca960de8b764d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aplicacao c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aplicacao c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
