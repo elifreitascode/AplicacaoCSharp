663601b28b46cd145f6f9ee25c1e00651f4a9253
Elizeu Freitas <elizeuferreiradefreitas@gmail.com>
2024-03-09 11:14:39 -0300

    update page login

--- Aplicacao PROVA/obj/Debug/net8.0/Aplicacao c#.AssemblyInfo.cs
+++ Aplicacao PROVA/obj/Debug/net8.0/Aplicacao c#.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aplicacao c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf58ad084753c972a5d9b124b0ca960de8b764d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3bb270831a8cc972743f8479ed0ad0a50125a05")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aplicacao c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aplicacao c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
