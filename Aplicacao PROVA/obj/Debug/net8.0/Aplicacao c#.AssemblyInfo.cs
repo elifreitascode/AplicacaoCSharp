16978f5a2393541c66fa0fdd34c0bc008dbf19fe
Elizeu Freitas <elizeuferreiradefreitas@gmail.com>
2024-03-09 15:22:18 -0300

    Mensagem do commit

--- Aplicacao PROVA/obj/Debug/net8.0/Aplicacao c#.AssemblyInfo.cs
+++ Aplicacao PROVA/obj/Debug/net8.0/Aplicacao c#.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aplicacao c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e55bcf069b4f9d04dbe2fd09003c9ea85d26536")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de90534f68e0f08af74f41212018b293fdbf9d9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aplicacao c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aplicacao c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
